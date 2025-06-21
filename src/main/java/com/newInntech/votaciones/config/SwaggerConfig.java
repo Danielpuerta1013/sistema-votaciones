20c1db39ca2d2bb6e20f898a4a5cbf5dfdff3b56
Daniel Puerta <daniwar3@gmail.com>
2025-06-20 22:44:51 -0500

    se agrega auth basica

--- src/main/java/com/newInntech/votaciones/config/SwaggerConfig.java
+++ src/main/java/com/newInntech/votaciones/config/SwaggerConfig.java
@@ -1,16 +1,30 @@
 package com.newInntech.votaciones.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
         info = @Info(
                 title = "API-REST - Sistema de votaciones",
                 version = "1.0.0",
-                description = "\"API RESTful para gestionar un sistema de votaciones." +
-                        "                \"Permite registrar votantes, candidatos y emitir votos. Esta API ha sido desarrollada como parte de una prueba técnica para la empresa **New Inntech**, e incluye estadísticas de votación como el total de votos por candidato, porcentaje de votos y más.\",",
+                description = """
+                        API RESTful para gestionar un sistema de votaciones. 
+                        Permite registrar votantes, candidatos y emitir votos. 
+                        Esta API ha sido desarrollada como parte de una prueba técnica para la empresa **New Inntech**.
+
+                        ### Autenticación:
+                        - Tipo: Autenticación básica (HTTP Basic Auth)
+                        - **Usuario:** `admin`
+                        - **Contraseña:** `1234`
+                        - Los endpoints de creación y eliminación requieren autenticación.
+
+                        También puedes consultar estadísticas generales de votaciones sin necesidad de autenticarte.
+                        """,
                 contact = @Contact(
                         name = "Daniel Puerta",
                         email = "daniwar3@gmail.com",
@@ -18,8 +32,10 @@ import io.swagger.v3.oas.annotations.servers.Server;
                 )
         ),
         servers = {
-                @Server(url = "http://localhost:8080/api",description = "servidor local")
-        }
+                @Server(url = "http://localhost:8080/api",description = "servidor local"),
+
+        }, security = {@SecurityRequirement(name = "basicAuth")}
 )
+@SecurityScheme(name = "basicAuth", type = SecuritySchemeType.HTTP, scheme = "basic")
 public class SwaggerConfig {
 }
